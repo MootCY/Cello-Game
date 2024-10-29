363be7725614334a575d26dc225519f8949867f2
MootCY <martywelsh021@gmail.com>
2024-10-29 15:00:05 +1100

    yep

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let ctx = canvas.getContext('2d');
 
 let celloX = canvas.width/2;
 let celloY = canvas.height/2;
-let celloXV = Math.floor(Math.random()*6)+1;
-let celloYV = Math.floor(Math.random()*6)+1;
+let celloXV = 0;
+let celloYV = 0;
 let celloWidth = 80;
 let celloHeight = 150;
 
@@ -14,20 +14,27 @@ let celloImg = new Image();
 celloImg.src="Cello.png";
 
 window.addEventListener('keydown',function(e){
-    if(celloXV != 0){
         if(e.code == 'ArrowUp'){
-            celloXV > -1? celloXV+= 1 : celloXV-= 1;
+            celloYV++;
         }
-    }
-    if(celloXV != 0){
-        if(e.code == 'ArrowUp'){
-            celloYV > -1? celloYV+= 1 : celloYV-= 1;
+        if(e.code == 'ArrowDown'){
+            celloYV--;
+        }
+        if(e.code == 'ArrowRight'){
+            celloXV++;
+        }
+        if(e.code == 'ArrowLeft'){
+            celloXV--;
         }
 });
 
 function update(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.drawImage(celloImg,celloX,celloY,celloWidth,celloHeight);
+    ctx.fillStyle = 'black';
+    ctx.font = "80px Arial";
+    ctx.fillText("X Velocity: "+celloXV,20,canvas.height-20);
+    ctx.fillText("Y Velocity: "+celloYV,20,canvas.height-20);
 
     celloX+= celloXV;
     celloY+= celloYV;
